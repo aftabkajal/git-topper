4d4ee30660369517e772464ef6bed4be4049e9da
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2018-10-01 22:31:02 +0600

    Update the application settings

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -101,7 +101,7 @@ namespace Web
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
